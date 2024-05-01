5411785c398df05e06d51b65bc1a67f7f5b55afd
MoyusiteruIori <y.meng1@outlook.com>
2024-05-01 12:45:40 +0800

    🩹chore: update cli tools

--- cli.py
+++ cli.py
@@ -2,11 +2,20 @@ from moss.moss import Moss
 from moss.log import setup_logging
 from moss.task_executor import TaskExecutor
 from moss.task_planner import Plan
+from moss.tools.background_eraser import BackgroundEraser
+from moss.tools.canny_detector import CannyDetector
+from moss.tools.depth_detector import DepthDetector
 from moss.tools.chitchat import ChitChat
 from moss.tools.image_generator import ImageGenerator
 from moss.tools.image_qa import ImageQA
-from moss.tools.summarizer import Summarizer
+from moss.tools.image_segmenter import ImageSegmenter
+from moss.tools.image_to_image_generator import ImageToImageGenerator
+from moss.tools.object_detector import ObjectDetector
+from moss.tools.object_replacer import ObjectReplacer
+from moss.tools.pose_detector import PoseDetector
+from moss.tools.sketch_refiner import SketchRefiner
 from moss.tools.speech_transcriber import SpeechTranscriber
+from moss.tools.summarizer import Summarizer
 from moss.tools.text_reader import TextReader
 from moss.tools.translator import Translator
 from langchain_openai import ChatOpenAI
@@ -19,13 +28,22 @@ setup_logging()
 if __name__ == "__main__":
 
     tools = [
+        BackgroundEraser(),
+        CannyDetector(),
+        DepthDetector(),
         ImageGenerator(),
         ImageQA(),
-        TextReader(),
+        ImageSegmenter(),
+        ImageToImageGenerator(),
+        ObjectDetector(),
+        ObjectReplacer(),
+        PoseDetector(),
+        SketchRefiner(),
         SpeechTranscriber(),
+        TextReader(),
         Translator(),
         Summarizer(),
-        ChitChat()
+        ChitChat(),
     ]
 
     agent = Moss(llm=ChatOpenAI(temperature=0, model="gpt-4"), tools=tools, verbose=False)
